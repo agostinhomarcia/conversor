2587576c9ff04e00298ad95177eeb8a96949bd5d
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-07-26 10:31:01 -0300

    using api

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.25
-const euro = 5.50
-const bitcoin = 0.000010
+// const dolar = 5.25
+// const euro = 5.50
+// const bitcoin = 0.000010
 
-convertValues = () => {
+convertValues = async  () => {
     const inputReal = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
     const valueConverted = document.getElementById('value-converted')
+
+    const data = await fetch(" https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL ").then(response => response.json())
+    
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
+    console.log(data)
 
     realValueText.innerHTML = Intl.NumberFormat('pt-BR', {
         style: "currency",
